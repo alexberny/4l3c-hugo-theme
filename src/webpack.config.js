64f4353f12f0d4f4f0d7aae478fcf037a127dfa8
Alessandro Bernardi <alexberny@yahoo.it>
2020-12-28 19:45:22 +0100

    Primo commit completo

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,55 +2,68 @@ const webpack = require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const AssetsPlugin = require('assets-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-    entry: './js/main.js',
-    output: {
-        path: path.resolve(__dirname, './../static/dist/'),
-        filename: 'js/[name].[contenthash].js'
-    },
-  
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
+module.exports = (env, argv) => {
+    const devMode = argv.mode !== 'production';
+
+    return {
+    
+        entry: './js/main.js',
+        output: {
+            path: path.resolve(__dirname, './../static/dist/'),
+            filename: devMode ? 'js/[name].js' : 'js/[name].[contenthash].js',
+        },
+    
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
                     }
-                }
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                        MiniCssExtractPlugin.loader, 
-                        'css-loader',
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1
+                            }
+                        },
                         'postcss-loader',
                         'sass-loader',
                     ]
-            }
+                }
 
+            ]
+        },
+        
+        resolve: {
+            extensions: ['.js', '.scss', '.css'],
+            modules: [path.resolve(__dirname, 'src'), 'node_modules']
+        },
+
+        plugins:[
+            new CleanWebpackPlugin(),
+            // new webpack.SourceMapDevToolPlugin(),
+            new MiniCssExtractPlugin({
+                filename: devMode ? 'css/[name].css' : 'css/[name].[contenthash].css',
+                chunkFilename: devMode ? '[id].css' : '[id].[contenthash].css',
+            }),
+
+            new AssetsPlugin({
+                filename: 'webpack_assets.json',
+                path: path.join(__dirname, '../data'),
+                removeFullPathAutoPrefix: true,
+                prettyPrint: true
+            }),
         ]
-    },
-    
-    resolve: {
-        extensions: ['.js', '.scss', '.css'],
-        modules: [path.resolve(__dirname, 'src'), 'node_modules']
-    },
-
-    plugins:[
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash].css',
-            chunkFilename: '[id].[contenthash].css'
-        }),
-
-        new AssetsPlugin({
-			filename: 'webpack_assets.json',
-            path: path.join(__dirname, '../data'),
-            removeFullPathAutoPrefix: true,
-			prettyPrint: true
-		}),
-    ]
+    }
 }
